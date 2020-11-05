0176edd89d01c6c459d0be831426472fa5bade36
sourav suman <saurav2fly@gmail.com>
2020-11-05 15:01:48 +0530

    added basic burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
-import Aux from '../../hoc/Auxiliary'
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger'
 class BurgerBuilder extends Component {
+  state = {
+    ingredients:{
+      salad:0,
+      bacon:0,
+      cheese:0,
+      meat:0
+    }
+  }
   render(){
   return (
     <Aux>
-    <div>
-    BurgerBuilder
-    </div>
+    <Burger ingredients={this.state.ingredients} />
     <div>
     Burger Controls
     </div>
